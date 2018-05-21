e5a5c25aa64f5b9d038b70ace14ed17bc418e66d
robie01 <robie.sun.rs@gmail.com>
2018-05-21 11:23:26 +0200

    firing arrow at the player's current x posiiton and sound clip

--- Assets/Scripts/ArrowScript.cs
+++ Assets/Scripts/ArrowScript.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class ArrowScript : MonoBehaviour {
 
+    private float arrowSpeed = 2.0f;
+    private bool canShootStickyArrow;
+
 	// Use this for initialization
 	void Start () {
-		
+        canShootStickyArrow = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        ShootArrow();
 	}
+
+    void ShootArrow() {
+        Vector3 temp = transform.position;
+        temp.y += arrowSpeed * Time.unscaledDeltaTime;
+        transform.position = temp;
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "TopBrick") {
+            gameObject.SetActive(false);
+        }
+    }
 }
--- Assets/Scripts/PlayerScripts.cs
+++ Assets/Scripts/PlayerScripts.cs
@@ -13,26 +13,69 @@ public class PlayerScripts : MonoBehaviour {
     [SerializeField]
     private Animator animator;
 
+    [SerializeField]
+    private GameObject[] arrows;
+
+    // to calculate the camera height
+    private float height;
+
+    private bool canWalk;
+
+    [SerializeField]
+    private AnimationClip clip;
+
+    [SerializeField]
+    private AudioClip shootclip;
+
+
+    // initialization
     void Awake()
     {
-       
+        float cameraHeight = Camera.main.orthographicSize;
+        // position the arrow exactly below the camera
+        height = -cameraHeight - 0.8f;
+        canWalk = true;
     }
 
     // Use this for initialization
     void Start () {
+        // for Player Object to have some mass and gravity, not to fall, equivalent to fix angle.
        GetComponent<Rigidbody2D>().freezeRotation = true;
 	}
 	
-	// Update is called once per frame
+    // Update is called once per frame
     // remember: Also used for method that is using transform.
-	void Update () {
-		
-	}
+    void Update()
+    {
+        shootTheArrow();
+    }
 
     // remember: Also used for method that is using physics.
     private void FixedUpdate()
     {
         PlayerWalkKeyBoard();
+    }
+
+    public void shootTheArrow() {
+
+        if(Input.GetMouseButtonDown(0)) {
+            StartCoroutine(PlayTheShootAnimation());
+
+            // firing arrows based on player's x
+            GameObject arrow1 = Instantiate(arrows[0]
+                                            , new Vector3(transform.position.x, height, 0),Quaternion.identity) as GameObject;
+        }
+
+    }
+
+    IEnumerator PlayTheShootAnimation() {
+        canWalk = false;
+        animator.Play("PlayerShoot");
+        AudioSource.PlayClipAtPoint(shootclip, transform.position);
+        yield return new WaitForSeconds(clip.length);
+        animator.SetBool("Shoot" ,false);
+        canWalk = true;
+        
     }
 
     void PlayerWalkKeyBoard() {
@@ -42,34 +85,41 @@ public class PlayerScripts : MonoBehaviour {
         // determining either we move to left side or right side.
         float h = Input.GetAxis("Horizontal");
 
-        if(h > 0){
-            // moving right
-            if(velocity < maxVelocity) {
-                force = speed;
-            }
-            Vector3 scale = transform.localScale;
-            scale.x = 1.0f;
-            transform.localScale = scale;
-
-            animator.SetBool("Walk", true);
-       
-        } else if (h < 0) {
-            // moving left
-            if (velocity < maxVelocity)
+        if(canWalk) {
+            if (h > 0)
             {
-                force = -speed;
+                // moving right
+                if (velocity < maxVelocity)
+                {
+                    force = speed;
+                }
+                Vector3 scale = transform.localScale;
+                scale.x = 1.0f;
+                transform.localScale = scale;
+
+                animator.SetBool("Walk", true);
+
             }
-            Vector3 scale = transform.localScale;
-            scale.x = -1.0f;
-            transform.localScale = scale;
+            else if (h < 0)
+            {
+                // moving left
+                if (velocity < maxVelocity)
+                {
+                    force = -speed;
+                }
+                Vector3 scale = transform.localScale;
+                scale.x = -1.0f;
+                transform.localScale = scale;
 
-            animator.SetBool("Walk", true);
+                animator.SetBool("Walk", true);
 
-        } else if(h == 0) {
-            
-            animator.SetBool("Walk", false);
+            }
+            else if (h == 0)
+            {
+                animator.SetBool("Walk", false);
+            }
+
+            myRigidBody.AddForce(new Vector2(force, 0));
         }
-
-        myRigidBody.AddForce(new Vector2(force, 0));
     }
 }

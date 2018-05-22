07b62c2537818ef9200e49da717e7195b83bdcc7
robie01 <robie.sun.rs@gmail.com>
2018-05-22 11:04:02 +0200

    sticky arrow should stick to brick but need to check to make it work

--- Assets/Scripts/ArrowScript.cs
+++ Assets/Scripts/ArrowScript.cs
@@ -7,6 +7,9 @@ public class ArrowScript : MonoBehaviour {
     private float arrowSpeed = 2.0f;
     private bool canShootStickyArrow;
 
+    [SerializeField]
+    private AudioClip stickyClip;
+
 	// Use this for initialization
 	void Start () {
         canShootStickyArrow = true;
@@ -14,8 +17,33 @@ public class ArrowScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        ShootArrow();
+        if (this.gameObject.tag == "FirstStickyArrow"){
+            if(canShootStickyArrow){
+                ShootArrow();
+            }
+        } else if(this.gameObject.tag == "SecondStickyArrow"){
+            if(canShootStickyArrow){
+                ShootArrow();
+            }
+        } else {
+            ShootArrow();
+        }
+      
 	}
+    IEnumerator ResetStickyArrow()
+    {
+        yield return new WaitForSeconds(2.5f);
+        if (this.gameObject.tag == "FirstStickyArrow")
+        {
+            PlayerScripts.instance.PlayerShootOnce(true);
+            this.gameObject.SetActive(false);
+        }
+        else if (this.gameObject.tag == "SecondStickyArrow")
+        {
+            PlayerScripts.instance.PlayerShootTwice(true);
+            this.gameObject.SetActive(false);
+        }
+    }
 
     void ShootArrow() {
         Vector3 temp = transform.position;
@@ -28,24 +56,47 @@ public class ArrowScript : MonoBehaviour {
         if(target.tag == "LargestBall" || target.tag == "LargeBall" || target.tag == "MediumBall"
            || target.tag == "SmallBall" || target.tag == "SmallestBall") {
 
-            if(gameObject.tag == "FirstArrow" || gameObject.tag == "FirstStickyArrow") {
+            if(gameObject.tag == "FirstStickyArrow" || gameObject.tag == "FirstArrow") {
                 PlayerScripts.instance.PlayerShootOnce(true);
-            } else if(gameObject.tag == "SecondArrow" || gameObject.tag == "SecondStickyArrow") {
+            } else if(gameObject.tag == "SecondStickyArrow" || gameObject.tag == "SecondArrow") {
                 PlayerScripts.instance.PlayerShootTwice(true);
             }
+
+            gameObject.SetActive(false);
+          
         } // if the arrow hits a ball
 
         if(target.tag == "TopBrick") {
 
-            if (gameObject.tag == "FirstArrow" || gameObject.tag == "FirstStickyArrow")
-            {
+            if(this.gameObject.tag == "FirstArrow"){
                 PlayerScripts.instance.PlayerShootOnce(true);
-            }
-            else if (gameObject.tag == "SecondArrow" || gameObject.tag == "SecondStickyArrow")
-            {
+                this.gameObject.SetActive(false);
+            } else if(this.gameObject.tag == "SecondArrow") {
                 PlayerScripts.instance.PlayerShootTwice(true);
+                this.gameObject.SetActive(false);
             }
-            gameObject.SetActive(false);
+            if (this.gameObject.tag == "FirstStickyArrow")
+            {
+                canShootStickyArrow = false;
+                Vector3 targetPos = target.transform.position;
+                Vector3 temp = transform.position;
+                targetPos.y -= 0.989f;
+                temp.y = targetPos.y;
+                transform.position = temp;
+                AudioSource.PlayClipAtPoint(stickyClip, transform.position);
+                StartCoroutine("ResetStickyArrow");
+            }
+            else if (this.gameObject.tag == "SecondStickyArrow")
+            {
+                canShootStickyArrow = false;
+                Vector3 targetPos = target.transform.position; // position of the brick
+                Vector3 temp = transform.position;
+                targetPos.y -= 0.989f;
+                temp.y = targetPos.y;
+                transform.position = temp;
+                AudioSource.PlayClipAtPoint(stickyClip, transform.position);
+                StartCoroutine("ResetStickyArrow");
+            }
 
         } // on trigger enter
 
